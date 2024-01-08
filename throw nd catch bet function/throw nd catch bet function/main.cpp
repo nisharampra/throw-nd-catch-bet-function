04e3bd6db94a8bb4dcadcfe84538deadab8d629d
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-09 00:13:41 +0800

    Add files via upload

---
+++ throw nd catch bet function/throw nd catch bet function/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  throw nd catch bet function
+//
+//  Created by Nisha Ramprasath on 7/11/23.
+//
+
+#include<iostream>
+using namespace std;
+    
+int division(int a,int b)
+{
+    if(b==0)
+        throw 1;
+    return a/b;
+}
+    
+int main()
+{
+    int x=10,y=2,z;
+    try
+    {
+        if(y==0)
+            throw 1;
+        z=x/y;
+        cout<<z<<endl;
+    }
+            
+    catch(int e)
+    {
+        cout<<"Division by Zero"<<e<<endl;
+    }
+    cout<<"Bye"<<endl;
+        
+}
+    
